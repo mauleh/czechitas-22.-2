a2a41196664247f9b1ddb45e39cdb8eee15b097e
mauleh <aulehlova.m@gmail.com>
2022-02-22 18:18:24 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,10 @@
-console.log ("pozdrav z ukolu jedna")
+console.log('pozdrav z ukolu jedna');
+
+let jmeno = 'Jiří';
+let vek = 32;
+let vaha = 100;
+let vyska = 1.9;
+
+console.log('Jmenuji se'+ jmeno + 'a je mi' + vek + "a vážím" + vaha + 'a moje BMI je' + vaha / vyska **2);
+
+
