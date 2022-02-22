7450dc4562b819bd30735a639bded2797375f688
mauleh <aulehlova.m@gmail.com>
2022-02-22 17:20:29 +0100

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// Import stylesheets
-import './style.css';
+//import "./style.css";
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log("Ahoj světe")
+
+import "./ukol1"
